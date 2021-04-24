41b73c1d7493ee3a985ad0ead7d011bca487d5ef
Xettri Aleen <pandapubgnp1@gmail.com>
2021-04-24 21:10:41 +0545

    gg

--- vue/solar.js
+++ vue/solar.js
@@ -1,5 +1,14 @@
-var rellax = new Rellax(".rellax", {
-  center: true,
-  breakpoints: [576, 768, 1201],
-  horizontal: true,
+const solarPlanets = new Vue({
+  el: "#solarplanets",
+  data: {
+    e: 0,
+  },
+  methods: {},
+  mounted: () => {
+    var rellax = new Rellax(".rellax", {
+      center: true,
+      breakpoints: [576, 768, 1201],
+      horizontal: true,
+    });
+  },
 });
